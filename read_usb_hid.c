bb0a610e90d917ac93eea0a0df535ebaa55a2734
Pasi Lammi <pasi.lammi@iki.fi>
2015-10-10 14:41:47 +0300

    autostyle

--- read_usb_hid.c
+++ read_usb_hid.c
@@ -22,111 +22,113 @@
 #define BUFLEN 512
 #define SERVER "127.0.0.1"
 #define PORT 8888
+#define MAXCODECHARS 10
 
 static int keepRunning = 1;
 
 void handler (int sig)
 {
-  printf ("\nexiting...(%d)n", sig);
-  keepRunning = 0;
+    printf ("\nexiting...(%d)n", sig);
+    keepRunning = 0;
 }
- 
+
 void perror_exit (char *error)
 {
-  perror (error);
-  handler (9);
+    perror (error);
+    handler (9);
 }
 
 int ourkey(int n) {
-	int ret = -1;
-	if (n > 1) {
-		if (n<11)
-			ret=(n-1);
-		if (n == 11)
-			ret=0;
-	}
-	return ret;
+    int ret = -1;
+    if (n > 1) {
+        if (n<11)
+            ret=(n-1);
+        if (n == 11)
+            ret=0;
+    }
+    return ret;
 }
- 
+
 int main (int argc, char *argv[])
 {
-  struct input_event event;
-  int fd; 
+    struct input_event event;
+    int fd;
 
-  char name[256] = "Unknown";
-  char *device = NULL;
-  int counter;
- 
-  //Setup check
-  if (argv[1] == NULL){
-      printf("Please specify (on the command line) the path to the dev event interface device\n");
-      exit (0);
+    char name[256] = "Unknown";
+    char *device = NULL;
+    int counter;
+
+    //Setup check
+    if (argv[1] == NULL) {
+        printf("Please specify (on the command line) the path to the dev event interface device\n");
+        exit (0);
     }
- 
-  if (argc > 1)
-    device = argv[1];
+
+    if (argc > 1)
+        device = argv[1];
 
 
-  struct sockaddr_in si_other;
-  int s, i, slen=sizeof(si_other);
-  char buf[BUFLEN];
-  char message[BUFLEN];
- 
-  if ( (s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+    struct sockaddr_in si_other;
+    int s, i, slen=sizeof(si_other);
+    char buf[BUFLEN];
+    char message[BUFLEN];
+
+    if ( (s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
         perror_exit("socket");
-  }
+    }
 
-  memset((char *) &si_other, 0, sizeof(si_other));
-  si_other.sin_family = AF_INET;
-  si_other.sin_port = htons(PORT);
-     
-  if (inet_aton(SERVER , &si_other.sin_addr) == 0)
-  {
+    memset((char *) &si_other, 0, sizeof(si_other));
+    si_other.sin_family = AF_INET;
+    si_other.sin_port = htons(PORT);
+
+    if (inet_aton(SERVER , &si_other.sin_addr) == 0)
+    {
         perror_exit("inet_aton() failed");
-  }
- 
-  signal(SIGINT, handler);
+    }
 
-  //Open Device
-  if ((fd = open (device, O_RDONLY)) == -1)
-    printf ("%s is not a vaild device.\n", device);
- 
-  ioctl (fd, EVIOCGNAME (sizeof (name)), name);
-  printf ("Reading From : %s (%s)\n", device, name);
- 
-  // only we are allow to read this device
-  ioctl (fd, EVIOCGRAB, 1);
+    signal(SIGINT, handler);
 
-  counter=0;
-  int code[10];
-  char x[10];
-  //count = 
-  while (read(fd, &event, sizeof(struct input_event)) > 0 && keepRunning) {
-    if (event.type != EV_KEY) continue;
-    if (event.value == ON_KEY_PRESS) {
-       if (event.code == KEY_ENTER) {
+    //Open Device
+    if ((fd = open (device, O_RDONLY)) == -1)
+        printf ("%s is not a vaild device.\n", device);
+
+    ioctl (fd, EVIOCGNAME (sizeof (name)), name);
+    printf ("Reading From : %s (%s)\n", device, name);
+
+    // only we are allow to read this device
+    ioctl (fd, EVIOCGRAB, 1);
+
+    counter=0;
+    unsigned char code[MAXCODECHARS];
+    while (read(fd, &event, sizeof(struct input_event)) > 0 && keepRunning) {
+        if (event.type != EV_KEY) continue;
+        if (event.value == ON_KEY_PRESS) {
+            if (event.code == KEY_ENTER) {
                 int i;
-		printf("ENTER\n");
-                for (i=0; i<10; i++) {
-                  printf ("%d", code[i]);
+                unsigned char msg[MAXCODECHARS];
+
+                printf("ENTER\n");
+                for (i=0; i<MAXCODECHARS; i++) {
+                    printf ("%d", code[i]);
+                    msg[i] = code[i] + 48;
                 }
                 printf ("\n");
 
-                sendto(s, code, sizeof(code) , 0 , (struct sockaddr *) &si_other, slen);
+                sendto(s, msg, sizeof(msg) , 0 , (struct sockaddr *) &si_other, slen);
 
-		counter=0;
-		memset(&code,0,10);
-       }
-       int key = ourkey(event.code);
-       if (key>-1 && key < 10 && counter<10) {
-           code[counter] = key;
-	   counter++;
-       }
+                counter=0;
+                memset(&code,0,MAXCODECHARS);
+            }
+            int key = ourkey(event.code);
+            if (key>-1 && key < 10 && counter < MAXCODECHARS) {
+                code[counter] = key;
+                counter++;
+            }
 
-    } 
-  }
-  printf ("Exit..\n");
-  close(s);
-  close(fd);
-  return 0;
-} 
+        }
+    }
+    printf ("Exit..\n");
+    close(s);
+    close(fd);
+    return 0;
+}
